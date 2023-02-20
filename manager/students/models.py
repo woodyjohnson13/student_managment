e2da6a89cab91c1ccc68eb4b8d7c3740efaa7463
woodyjohnson13 <johnsonwoody13@gmail.com>
2023-02-20 22:26:27 +0400

    bootswatch and bootstrap connected

--- manager/students/models.py
+++ manager/students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    student_number=models.PositiveBigIntegerField(primary_key=True)
+    student_name=models.CharField(max_length=50)
+    student_last_name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    field_of_study=models.CharField(max_length=50)
+    gpa=models.FloatField()
+    
+    def __str__(self):
+        return f"Student {self.student_name} {self.student_last_name}, {self.field_of_study}, {self.gpa}"

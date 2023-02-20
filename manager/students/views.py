08537a5178193de573d7d299ccf63f10a6ed300d
woodyjohnson13 <johnsonwoody13@gmail.com>
2023-02-20 21:24:35 +0400

    init

--- manager/students/views.py
+++ manager/students/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'student/index.html')
+    return render(request, 'students/index.html')
